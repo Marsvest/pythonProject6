1ad8dd5d62360305f7ab7a4ccecf5bd4b3005516
Marsvest <alajh.akbar@gmail.com>
2024-06-10 23:33:56 +1000

    bot

--- bot.py
+++ bot.py
@@ -19,8 +19,8 @@ class ChatBot:
 
         self.model = AutoModelForCausalLM.from_pretrained(
             "microsoft/Phi-3-mini-4k-instruct",
-            device_map="cuda",
-            torch_dtype="auto",
+            device_map="cpu",
+            torch_dtype=torch.float16,
             trust_remote_code=True,
         )
         self.tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
